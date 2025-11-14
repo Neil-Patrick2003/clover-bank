f27ced11c52bb16eccc09d48d77550809715f0c0
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-11-14 20:58:48 +0800

    wip

--- database/migrations/2025_10_14_172544_create_kyc_profiles_table.php
+++ database/migrations/2025_10_14_172544_create_kyc_profiles_table.php
@@ -1,32 +1,32 @@
-<?php
+        <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Migrations\Migration;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('kyc_profiles', function (Blueprint $table) {
-            $table->id(); // kyc_id
-            $table->foreignId('user_id')->unique()->constrained('users')->cascadeOnDelete();
-            $table->enum('kyc_level', ['basic','standard','enhanced'])->default('basic');
-            $table->enum('id_type', ['passport','national_id','driver_license','sss','umid','other'])->nullable();
-            $table->string('id_number', 128)->nullable();
-            $table->date('id_expiry')->nullable();
-            $table->timestamps();
-        });
-    }
+        return new class extends Migration
+        {
+            /**
+             * Run the migrations.
+             */
+            public function up(): void
+            {
+                Schema::create('kyc_profiles', function (Blueprint $table) {    
+                    $table->id(); // kyc_id
+                    $table->foreignId('user_id')->unique()->constrained('users')->cascadeOnDelete();
+                    $table->enum('kyc_level', ['basic','standard','enhanced'])->default('basic');
+                    $table->enum('id_type', ['passport','national_id','driver_license','sss','umid','other'])->nullable();
+                    $table->string('id_number', 128)->nullable();
+                    $table->date('id_expiry')->nullable();
+                    $table->timestamps();
+                });
+            }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('kyc_profiles');
-    }
-};
+            /**
+             * Reverse the migrations.
+             */
+            public function down(): void
+            {
+                Schema::dropIfExists('kyc_profiles');
+            }
+        };
